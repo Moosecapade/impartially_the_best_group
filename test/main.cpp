6b37e0b0168c1cdd55f9fcbd0a4598371d388354
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2023-12-06 19:31:19 -0800

    Add files via upload

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,8 @@
 //#include "heap.h"
 using namespace std;
 
+bool notInVector(int val, vector<int> vec);
+
 int main(int argc, char* argv[])
 {
     // Header
@@ -13,9 +15,9 @@ int main(int argc, char* argv[])
     cout << "Course: CS 311 (Data Structures and Algorithms)" << endl;
     cout << "Description: Program to find the shortest route between cities" << endl;
     cout << "-------------------------------------------------------------------" << endl;
-    /*
+    
     ifstream infile;
-    Graph cityGraph;
+    Graph cityGraph;    // empty graph
 
     // Read vertices from input file (cities)
     infile.open("city.txt");
@@ -31,7 +33,7 @@ int main(int argc, char* argv[])
         int population;
         int elevation;
 
-        while (infile)
+        while (!infile.eof())
         {
             // uncomment couts for testing
             //cout << "Getting input: \n";
@@ -49,13 +51,17 @@ int main(int argc, char* argv[])
             infile >> elevation;
             //cout << "Elevation: " << elevation << endl;
             
-            
-            Vertex city;
+            // construct a vertex from read data
+            Vertex city(id, cityCode, cityName, population, elevation, -1, false, 0.0);
+            /*
             city.id = id;
             city.cityCode = cityCode;
             city.name = cityName;
             city.population = population;
             city.elevation = elevation;
+            */
+           
+            // add vertex to graph
             cityGraph.addVertex(city);
         }
     }
@@ -88,40 +94,66 @@ int main(int argc, char* argv[])
     bool valid2 = false;
     int index1 = 0;
     int index2 = 0;
-        for (int i = 0; i < (int)cityGraph.vertices.size(); i++){ //Ryan
-            counter++;
-            if (abb1 == cityGraph.vertices[i].cityCode){
-                valid1 = true;
-                index1 = i;
-            }
-            if (abb2 == cityGraph.vertices[i].cityCode){
-                valid2 = true;
-                index2 = i;
-            }
-            if (valid1 == true && valid2 == true){
-                break;
-            }
-    
-
-
-            
-        } //Ryan
-        if (valid1 == false ){
-            cout << "Invalid City Code: " << abb1; //Ryan
+    for (int i = 0; i < (int)cityGraph.vertices.size(); i++){ //Ryan
+        counter++;
+        if (abb1 == cityGraph.vertices[i].cityCode){
+            valid1 = true;
+            index1 = i;
         }
-        if (valid2 == false ){
-            cout << "Invalid City Code: " << abb2; //Ryan
+        if (abb2 == cityGraph.vertices[i].cityCode){
+            valid2 = true;
+            index2 = i;
         }
+        if (valid1 == true && valid2 == true){
+            break;
+        }   
+    } //Ryan
+    if (valid1 == false && valid2 == true){
+        cout << "Invalid City Code: " << abb1 << endl; //Ryan
+    }else if (valid2 == false && valid1 == true){
+        cout << "Invalid City Code: " << abb2 << endl; //Ryan
+    }else if (valid1 == false && valid2 == false){
+        cout << "Invalid City Codes: " << abb1 << " and " << abb2 << endl;
+    }
+    if(valid1 == false || valid2 == false){
+        cout << "usage: prog9 city_code1 city_code2" << endl;
+        cout << "valid city codes follow:" << endl;
+        for(int i = 0; i < (int)cityGraph.vertices.size(); i++){
+            cout << cityGraph.vertices.at(i).cityCode << endl;
+        }
+        return 0;
+    } // at this point if the program is still running we have two valid city codes
+    // now checking a path exists between the two cities
+
+    vector<int> BFS = cityGraph.BreadthFirstSearch(index1);
+
+    if(notInVector(index2, BFS)){
+        cout << "No route from " << cityGraph.vertices.at(index1).name << " to " << cityGraph.vertices.at(index2).name << endl;
+        cout << "usage: prog9 city_code1 city_code2" << endl;
+        cout << "valid city codes follow:" << endl;
+        for(int i = 0; i < (int)cityGraph.vertices.size(); i++){
+            cout << cityGraph.vertices.at(i).cityCode << endl;
+        }
+        return 0;
+    }// if program has survived, the two city codes exist in the graph AND a path exists between them.
+
+    cout << "From City: " << cityGraph.vertices.at(index1).name << ", population " << cityGraph.vertices.at(index1).population << ", elevation " << cityGraph.vertices.at(index1).elevation << endl;
+    cout << "From City: " << cityGraph.vertices.at(index2).name << ", population " << cityGraph.vertices.at(index2).population << ", elevation " << cityGraph.vertices.at(index2).elevation << endl;
+        
 
     double tempDistance = 0;
     vector<Vertex> newcitypath;
     newcitypath = cityGraph.djikstraAlgorithm(cityGraph.vertices[index1], cityGraph.vertices[index2], tempDistance);
 
-    for(int i = 0; i < (int)newcitypath.size(); i++){
-        cout << newcitypath.at(i).cityCode << " "; 
+    cout << "The shortest distance from " << cityGraph.vertices.at(index1).name << " to " << cityGraph.vertices.at(index2).name << " is " << tempDistance << " through the route:" ;
+
+    cout << newcitypath.at(0).name; 
+    for(int i = 1; i < (int)newcitypath.size(); i++){
+        cout << "->" << newcitypath.at(i).name; 
     }
     cout << endl;
-    */
+    
+    /*
     Graph map;
     Vertex A(0, "A", "", 0, 0,-1, false, 0.0);
     Vertex B(1, "B", "", 0, 0,-1, false, 0.0);
@@ -147,6 +179,15 @@ int main(int argc, char* argv[])
         cout << shortPath.at(i).cityCode << " " ;
     }
     cout << endl;
+    */
 
     return 0;
 }
+
+bool notInVector(int val, vector<int> vec){
+    for(int i = 0; i < (int)vec.size(); i++){
+        if(vec.at(i) == val) return false;
+    }
+    return true;
+}
+
